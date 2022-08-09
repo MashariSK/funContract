0548dc7be31e7b79ff23285c7158842942de9fd2
MashariSK <mashari.saleh@hotmail.com>
2022-08-09 19:19:04 +0300

    missing

--- contracts/test/VRFCoordinatorV2Mock.sol
+++ contracts/test/VRFCoordinatorV2Mock.sol
@@ -2,9 +2,9 @@
 // A mock for testing code that relies on VRFCoordinatorV2.
 pragma solidity ^0.8.4;
 
-import "../interfaces/LinkTokenInterface.sol";
-import "../interfaces/VRFCoordinatorV2Interface.sol";
-import "../VRFConsumerBaseV2.sol";
+// import "../interfaces/LinkTokenInterface.sol";
+// import "../interfaces/VRFCoordinatorV2Interface.sol";
+// import "../VRFConsumerBaseV2.sol";
 
 contract VRFCoordinatorV2Mock is VRFCoordinatorV2Interface {
   uint96 public immutable BASE_FEE;
